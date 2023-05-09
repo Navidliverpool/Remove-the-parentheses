a63c3ef58b5d249de2abca5caf4b5bcbbbf44f89
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-05-09 17:08:01 +0100

    1.js not passing the tests.

--- 1.js
+++ 1.js
@@ -1,3 +1,20 @@
-function removeParentheses(s){
-  
+function removeParentheses(s) {
+  let r = "";
+  let a = []
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+        a[0] = i
+    }
+    if (s[i] === ")") {
+        a[1] = i
+    }
+  }
+  for (let i = 0; i < a[0]; i++) {
+r+= s[i]  
 }
+for (let i = a[1] + 1; i < s.length; i++) {
+    r+= s[i]  
+    }
+  return r
+}
+console.log(removeParentheses("example(unwanted thing)example"))
